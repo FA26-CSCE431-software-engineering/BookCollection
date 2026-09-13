2632f0b4bbde6e08af879d3798d1e94cbfea0fb4
Richard_Ngo <richard.ng135@gmail.com>
2026-09-13 15:13:23 -0500

    Completed Chapter 2 on LinkedIn

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,7 @@
 class TasksController < ApplicationController
   def index
+    @tasks = ['Fold laundry', 'Sweep porch', 'Wash dishes', 'Mow lawn']
+    @page = params[:page].to_i
   end
 
   def new
