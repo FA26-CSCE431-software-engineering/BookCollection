ef44e0b1bb0f7d64f55205dbcda5baf2fa99a6ce
Richard_Ngo <richard.ng135@gmail.com>
2026-09-14 17:30:04 -0500

    Completed the things for part 4

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
+  resources :user_books
+  resources :users
   resources :books do
     member do
       get :confirm_delete
     end
   end
-  
-  root "books#index"
+
+  root 'user_books#index'
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
