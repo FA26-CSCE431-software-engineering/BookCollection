b4fa105d81a9a55770e5f3887a7bbba77892aa5e
Richard_Ngo <richard.ng135@gmail.com>
2026-09-14 11:13:57 -0500

    Created the seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,23 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+books = [
+  { title: 'Harry Potter and the Sorcerer\'s Stone', author: 'J.K. Rowling',           price: 19.99, published_date: Date.new(1997, 6, 26) },
+  { title: 'The Hobbit',                             author: 'J.R.R. Tolkien',         price: 14.99, published_date: Date.new(1937, 9, 21) },
+  { title: '1984',                                   author: 'George Orwell',          price: 12.99, published_date: Date.new(1949, 6, 8) },
+  { title: 'Pride and Prejudice',                    author: 'Jane Austen',            price:  9.99, published_date: Date.new(1813, 1, 28) },
+  { title: 'The Great Gatsby',                       author: 'F. Scott Fitzgerald',    price: 10.99, published_date: Date.new(1925, 4, 10) }
+]
+
+# Seed the data above based on the example, albeit with more due to the extra attributes from Part 2 of Book Collection
+books.each do |attrs|
+  Book.find_or_create_by(title: attrs[:title]) do |b|
+    b.author         = attrs[:author]
+    b.price          = attrs[:price]
+    b.published_date = attrs[:published_date]
+  end
+end
+
+# Debug line to see if all is well
+puts "Seeded #{Book.count} books in #{Rails.env}"
