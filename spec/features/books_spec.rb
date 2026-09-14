b7f76053951be7f33510f54e763723b6c6f8cd64
Richard_Ngo <richard.ng135@gmail.com>
2026-09-14 00:24:48 -0500

    Turns out, the problem was that I forgot to update one of my tests

--- spec/features/books_spec.rb
+++ spec/features/books_spec.rb
@@ -4,6 +4,11 @@ RSpec.describe 'Creating a book', type: :feature do
   scenario 'valid inputs' do
     visit new_book_path
     fill_in 'book[title]', with: 'Harry Potter'
+    fill_in 'book[author]', with: 'J.K. Rowling'
+    fill_in 'book[price]', with: '19.99'
+    select '1997', from: 'book_published_date_1i'
+    select 'June', from: 'book_published_date_2i'
+    select '26', from: 'book_published_date_3i'
     click_on 'Create Book'
 
     expect(page).to have_content('Book was successfully created.')
